0b37f5c5197c3b10c448f7e88ef3a58806288919
Noamhotcoffee11 <117226467+Noamhotcoffee11@users.noreply.github.com>
2025-02-05 20:34:49 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,47 +1,46 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
-from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from plot import plot_training as plt
 import tensorflow as tf
-
+from gc import collect
 
 def create_model(train, val):
 
     early_stopping = EarlyStopping(monitor="val_loss",
-                                   patience=5,         
+                                   patience=3,         
                                    restore_best_weights=True,  
                                    verbose=1)
 
+    tf.keras.backend.clear_session()
     
     model = Sequential()
     
-    model.add(Conv2D(32, (3,3), 1, padding='same', activation='relu', input_shape=(128, 128, 3)))
+    model.add(Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(128, 128, 3)))
+
+    model.add(BatchNormalization())
+
+    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
     
+    model.add(MaxPooling2D())
     model.add(BatchNormalization())
     
     model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
-
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D())
     
-    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
-    
-    model.add(BatchNormalization())
-
     model.add(MaxPooling2D())
+    model.add(BatchNormalization())
 
     model.add(Dropout(0.3))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.01)))
+    model.add(Dense(128, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-
+    collect()
     model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
     model.summary()
 
-    hist = model.fit(train, epochs=20, 
+    hist = model.fit(train, epochs=7, 
                      validation_data=val, 
                      callbacks=[early_stopping])
 
