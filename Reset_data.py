c922572999ab341c3da8f9c17bdf641b692e13a4
Noamhotcoffee11 <117226467+Noamhotcoffee11@users.noreply.github.com>
2025-02-09 11:40:42 +0200

    Update Reset_data.py

--- Reset_data.py
+++ Reset_data.py
@@ -4,10 +4,8 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import shutil
 from gc import collect
 import os
-from glob import glob
 
 def create_directory(dataset_path: str) -> str:
     '''
@@ -22,11 +20,9 @@ def create_directory(dataset_path: str) -> str:
     collect()
     dataset_path=os.path.expanduser(dataset_path)
     
-    if os.path.exists(f'{dataset_path}/classes'):
-        shutil.rmtree(f'{dataset_path}/classes')
-
-    os.mkdir(f'{dataset_path}/classes')
-    os.mkdir(f'{dataset_path}/classes/ai')
-    os.mkdir(f'{dataset_path}/classes/real')
+    if not os.path.exists(f'{dataset_path}/classes'):
+        os.mkdir(f'{dataset_path}/classes')
+        os.mkdir(f'{dataset_path}/classes/ai')
+        os.mkdir(f'{dataset_path}/classes/real')
 
     return dataset_path
