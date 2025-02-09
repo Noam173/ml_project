0a50127b4d3c92d0124450077eb0be29f8d4c0c6
Noamhotcoffee11 <117226467+Noamhotcoffee11@users.noreply.github.com>
2025-02-09 10:47:55 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, GlobalAveragePooling2D
 from keras.callbacks import EarlyStopping
 from plot import plot_training as plt
 import tensorflow as tf
-from gc import collect
+
 
 def create_model(train, val):
 
@@ -12,36 +12,32 @@ def create_model(train, val):
                                    restore_best_weights=True,  
                                    verbose=1)
 
-    tf.keras.backend.clear_session()
     
     model = Sequential()
     
-    model.add(Conv2D(64, (3,3), padding='same', activation='relu', input_shape=(224, 224, 3)))
-
+    model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+    
     model.add(MaxPooling2D())
+    
+    model.add(Conv2D(64, (3,3), activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D())
+    
+    model.add(Conv2D(16, (3,3), activation='relu'))
     
     model.add(MaxPooling2D())
-
-    model.add(Dropout(0.2))    
-    model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
     
-    model.add(MaxPooling2D())
-
-    model.add(Dropout(0.3))
-    model.add(Flatten())
-    model.add(Dense(224, activation='relu'))
+    model.add(GlobalAveragePooling2D())
+    model.add(Dropout(0.4))
+    model.add(Dense(256, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
+
 
     model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
-    
-    collect()
-
     model.summary()
 
-    hist = model.fit(train, epochs=10, 
+    hist = model.fit(train, epochs=20, 
                      validation_data=val, 
                      callbacks=[early_stopping])
 
