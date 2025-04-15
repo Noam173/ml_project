e1d12e0bfbab703fd71dd6cb28972d4844bfdf6f
Noam <Noamiko173@gmail.com>
2025-04-15 19:59:01 +0300

    idk

--- predict_images.py
+++ predict_images.py
@@ -11,7 +11,7 @@ def predict_image(img_dir: str) -> None:
             img = tf.image.decode_png(img, channels=3)
         except:
             img = tf.image.decode_bmp(img, channels=3)
-        img = tf.expand_dims(img , axis=0)
+        img = tf.expand_dims(img, axis=0)
         img = tf.image.resize(img, (224, 224)) / 255
         return img
 
