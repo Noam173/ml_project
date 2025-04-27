4054d83ff896dad8d627403497ddfb43ddefdcb0
idk <idk>
2025-04-27 09:37:46 +0300

    idk

---
+++ .ipynb_checkpoints/Data_Manipulation-checkpoint.py
@@ -0,0 +1,55 @@
+import shutil
+from gc import collect
+from glob import glob
+import pandas as pd
+import tensorflow as tf
+from Reset_data import create_directory
+
+
+def split_data(path: str) -> str:
+    """
+    Parameters
+    ----------
+    train_path : string.
+        the path to the original dataset's DataFrame file to prepare it for splitting.
+
+    Returns
+    -------
+    None.
+
+    """
+    dataset_path, flag = create_directory(path)
+    train_path_csv = glob(f"{dataset_path}/*.csv")[0]
+    if flag:
+        path = pd.read_csv(train_path_csv)
+
+        real = path[path["label"] == 0].file_name
+        ai = path[path["label"] == 1].file_name
+
+        for i in real:
+            shutil.copy(f"{dataset_path}/{i}", f"{dataset_path}/classes/real")
+
+        for i in ai:
+            shutil.copy(f"{dataset_path}/{i}", f"{dataset_path}/classes/ai")
+    print("done")
+
+    collect()
+
+
+def preprocess_data(train_path: str, model) -> None:
+    data = tf.keras.utils.image_dataset_from_directory(
+        train_path, shuffle=True, seed=0, image_size=(224, 224)
+    ).map(lambda x, y: (x / 255.0, y))
+
+    size = len(data)
+
+    train_size = round(size * 0.6)
+    val_size = round(size * 0.2)
+
+    train = data.take(train_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    val = data.skip(train_size).take(val_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    test = data.skip(train_size + val_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    model = tf.keras.models.load_model(model)
+
+    print(model.evaluate(test))
