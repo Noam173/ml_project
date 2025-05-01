a060e8229333b2493767b4037c6e4747a4ff3076
Noam Cohen <Noamiko173@gmail.com>
2025-05-01 03:20:10 +0300

    idk

--- Reset_data.py
+++ Reset_data.py
@@ -1,16 +1,15 @@
 from gc import collect
 from pathlib import Path
 
-
-def create_directory(path: str) -> str:
+def create_directory(path: str) -> tuple[str, bool]:
     collect()
     dataset_path = Path(path).parent.as_posix()
     new_path = Path(f"{dataset_path}/classes")
-    Flag = False
+    flag = False
     if not new_path.exists():
-        Path.mkdir(new_path)
-        Path.mkdir(f"{new_path}/ai")
-        Path.mkdir(f"{new_path}/real")
-        Flag = True
+        new_path.mkdir(parents=True, exist_ok=True)
+        (new_path / "ai").mkdir(parents=True, exist_ok=True)
+        (new_path / "real").mkdir(parents=True, exist_ok=True)
+        flag = True
 
-    return dataset_path, Flag
+    return dataset_path, flag
